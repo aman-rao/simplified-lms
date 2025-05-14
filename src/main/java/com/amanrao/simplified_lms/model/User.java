076210a7734c4c7a5e69234f7c2c35245a2d1e54
amanrao4 <rao.am@northeastern.edu>
2025-05-14 12:14:18 -0400

    Day 1: Project setup, MySQL file configuration, User Entity and repository created

---
+++ src/main/java/com/amanrao/simplified_lms/model/User.java
@@ -0,0 +1,34 @@
+package com.amanrao.simplified_lms.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public enum Role {
+        STUDENT,
+        INSTRUCTOR,
+        ADMIN
+    }
+}
---
+++ src/main/java/com/amanrao/simplified_lms/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.amanrao.simplified_lms.repository;
+
+import com.amanrao.simplified_lms.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
