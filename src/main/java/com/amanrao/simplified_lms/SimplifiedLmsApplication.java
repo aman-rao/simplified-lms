e5a23d86d1885f24f12bd855e876a8fe43e9a116
amanrao4 <rao.am@northeastern.edu>
2025-05-14 11:35:50 -0400

    Initial commit for Spring Boot project setup

---
+++ src/main/java/com/amanrao/simplified_lms/SimplifiedLmsApplication.java
@@ -0,0 +1,13 @@
+package com.amanrao.simplified_lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedLmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedLmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amanrao/simplified_lms/SimplifiedLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amanrao.simplified_lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedLmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
