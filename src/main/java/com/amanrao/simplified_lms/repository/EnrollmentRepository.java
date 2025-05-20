483d3de774fb9da54d01f5412befe0c716f8817c
amanrao4 <aman6935@gmail.com>
2025-05-20 16:36:49 -0400

    Implemented instructor view for enrolled students with UI and backend support

--- src/main/java/com/amanrao/simplified_lms/repository/EnrollmentRepository.java
+++ src/main/java/com/amanrao/simplified_lms/repository/EnrollmentRepository.java
@@ -3,6 +3,8 @@ package com.amanrao.simplified_lms.repository;
 import com.amanrao.simplified_lms.model.Enrollment;
 import com.amanrao.simplified_lms.model.User;
 import com.amanrao.simplified_lms.model.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -13,4 +15,7 @@ public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
     List<Enrollment> findByStudent(User student);
 
     Optional<Enrollment> findByStudentAndCourse(User student, Course course);
+    Page<Enrollment> findByStudent(User student, Pageable pageable);
+    List<Enrollment> findByCourse(Course course);
+
 }
--- src/main/java/com/amanrao/simplified_lms/service/EnrollmentService.java
+++ src/main/java/com/amanrao/simplified_lms/service/EnrollmentService.java
@@ -3,11 +3,16 @@ package com.amanrao.simplified_lms.service;
 import com.amanrao.simplified_lms.model.Course;
 import com.amanrao.simplified_lms.model.Enrollment;
 import com.amanrao.simplified_lms.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface EnrollmentService {
     void enrollStudentInCourse(User student, Course course);
-    List<Enrollment> getEnrollmentsForStudent(User student);
     boolean isEnrolled(User student, Course course);
+    Page<Enrollment> getEnrollmentsForStudent(User student, Pageable pageable);
+    List<Enrollment> getEnrollmentsForCourse(Course course);
+
+
 }
